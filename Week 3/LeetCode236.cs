df49b162683f056d1971ba37ed7d6bf712f772d6
Iris <qxy1464@Iriss-MacBook-Pro.local>
2021-08-25 16:51:08 +0800

    update code

--- Week 3/LeetCode236.cs
+++ Week 3/LeetCode236.cs
@@ -5,6 +5,16 @@ namespace algorithmHomework{
     二叉树的最近公共祖先
     */
     public class LeetCode236{
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root ==null || root == p|| root==q) return root;
+            TreeNode left=LowestCommonAncestor(root.left,p,q);
+            TreeNode right=LowestCommonAncestor(root.right, p, q);
+
+            if(left==null) return right;
+            if(right==null) return left;
+
+            return root;
+        }
 
     }
 }
---
+++ Week 4/LeetCode122.cs
@@ -0,0 +1,20 @@
+using System.Collections.Generic;
+using System.Math;
+
+namespace algorithmHomework{
+    /*
+    122. 买卖股票的最佳时机 II
+    */
+
+    public class LeetCode122{
+        public int MaxProfit(int[] prices) {
+            int result=0;
+            if(prices.Length<0) return result;
+            for(int i=1;i<prices.Length;i++)
+            {
+                result+=Max(prices[i]-prices[i-1],0);
+            }
+            return result;
+        }
+    }
+}
