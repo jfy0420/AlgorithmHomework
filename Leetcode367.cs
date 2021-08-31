2499689bf56311dc92936f8dedb030fd5f30d0f2
Iris <qxy1464@Iriss-MacBook-Pro.local>
2021-08-31 18:32:33 +0800

    continue

---
+++ Leetcode367.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+
+namespace algorithmHomework{
+    /*有效平方数：
+    给定一个 正整数 num ，编写一个函数，如果 num 是一个完全平方数，则返回 true ，否则返回 false 。
+
+    进阶：不要 使用任何内置的库函数，如  sqrt 。
+    */
+    public class LeetCode367 {
+
+        //Binary search
+        public bool IsPerfectSquare(int num) {
+            if (num == 1) {
+                return true;
+            }
+
+        long left=2, right=num/2, guess, x;
+        while(left<=right){
+            x=left+(right-left)/2;
+            guess=x*x;
+            if(guess==num)
+                return true;
+            
+                if(guess>num)
+                    right=x-1;
+                else
+                    left=left+1;
+        }
+
+        return false;
+        }
+
+        //牛顿法f(x)=x*x−num=0 的根
+        public bool IsPerfectSquare2(int num) {
+            if (num == 1) return true;
+
+            long x = num / 2;
+            while (x * x > num) {
+                x = (x + num / x) / 2;
+            }
+            return (x * x == num);
+
+        }
+    }
+}
--- Week 4/LeetCode127.cs
+++ Week 4/LeetCode127.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace algorithmHomework{
@@ -12,10 +13,129 @@ namespace algorithmHomework{
 给你两个单词 beginWord 和 endWord 和一个字典 wordList ，找到从 beginWord 到 endWord 的 最短转换序列 中的 单词数目 。如果不存在这样的转换序列，返回 0。
 */
     public class LeetCode127{
-        // this is BFS
+        // 用26个字母
         public int LadderLength(string beginWord, string endWord, IList<string> wordList) {
-            
+            HashSet<string> wordSet=new HashSet<string>(wordList);
+
+            if(wordList.Count==0||!wordSet.Contains(endWord)){
+                return 0;
+            }
+            wordSet.Remove(beginWord);
+
+            //广度搜索：使用队列和visited
+            Queue<string> queue=new Queue<string>();
+            queue.Enqueue(beginWord);
+
+            //BFS
+             int step=0;
+            while(queue.Count>0){
+                int size=queue.Count;
+                while(size-->0){
+                    string currentWord=queue.Dequeue();
+
+                    if(currentWord==endWord)
+                        return step+1;
+                    for(int i=0;i<currentWord.Length;i++){//遍历所有字符
+                        for(char c='a';c<='z';c++){
+                            string newWord=currentWord.Substring(0,i)+c+currentWord.Substring(i+1);
+                            if(wordSet.Contains(newWord)){
+                                queue.Enqueue(newWord);
+                                
+                                wordSet.Remove(newWord);
+                            }
+                        }
+                    }
+                }
+                step++;    
+            }
             return 0;
+        }
+
+        public int LadderLength1(string beginWord, string endWord, IList<string> wordList) {
+            HashSet<string> wordSet=new HashSet<string>(wordList);
+
+            if(wordList.Count==0||!wordSet.Contains(endWord)){
+                return 0;
+            }
+            wordSet.Remove(beginWord);
+
+            //广度搜索：使用队列和visited
+            Queue<string> queue=new Queue<string>();
+            queue.Enqueue(beginWord);
+
+            HashSet<string> visited=new HashSet<string>();
+            visited.Add(beginWord);
+
+            //BFS
+            int step=1;
+            while(queue.Count>0){
+                int currentSize=queue.Count;
+                for(int i=0;i<currentSize;i++){
+                    string currentWord=queue.Dequeue();
+
+                    foreach(var s in wordList){
+                        if(visited.Contains(s))
+                            continue;
+                        
+                        //不能跳过的
+                        if(!CanConvert(currentWord, s))
+                            continue;
+                        
+                        if(s==endWord){
+                            return step++;
+                        }
+                        //保存访问过的单词，同时把单词放进队列，用于下一层的访问
+                        visited.Add(s);
+                        queue.Enqueue(s);
+                    }
+
+                    //if(ChangeWordEveryOneLetter(currentWord,endWord,queue,visited,wordSet))
+                    //    return step+1;
+                }
+                //step++;
+            }
+            return 0;
+        }
+
+        //这个超时了
+        private bool CanConvert(string s1, string s2)
+        {
+            if (s1.Length != s2.Length) return false;
+            int count = 0;
+            for (int i = 0; i < s1.Length; ++i) {
+                if (s1[i]!= s2[i]) {
+                    ++count;
+                    if (count > 1) {
+                        return false;
+                    }
+                }
+             }
+            return count == 1;
+        }
+
+        private bool ChangeWordEveryOneLetter(string currentWord, string endWord, Queue<string> queue, HashSet<string> visited, HashSet<string> wordSet)
+        {
+            char[] charArray=currentWord.ToCharArray();
+            for(int i=0;i<endWord.Length;i++){
+                char originChar=charArray[i];
+                for(char k='a';k<='z';k++){
+                    if(k==originChar){
+                        continue;
+                    }
+                    charArray[i]=k;
+                    string nextWord=charArray.ToString();
+                    if(wordSet.Contains(nextWord)){
+                        if(nextWord==endWord)
+                            return true;
+                    }
+                    if(!visited.Contains(nextWord)){
+                        queue.Enqueue(nextWord);
+                        visited.Add(nextWord);
+                    }
+                }
+                charArray[i]=originChar;
+            }
+            return false;
         }
     }
 }
