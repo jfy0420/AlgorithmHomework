ab0b4963370945e3459abcf8ac855d8ec39cd8cb
Iris <qxy1464@Iriss-MacBook-Pro.local>
2021-10-11 10:59:23 +0800

    week 9 updateupdate

--- Week 9/LeetCode205.cs
+++ Week 9/LeetCode205.cs
@@ -12,5 +12,27 @@ namespace algorithmHomework{
             }
             return true;
         }
+
+        //效率更差
+        public bool IsIsomorphic2(string s, string t) {
+            return IsIsomorphicHelper(s,t)&&IsIsomorphicHelper(t,s);
+        }
+        private bool IsIsomorphicHelper(string s, string t){
+        Dictionary<char,char> dic=new Dictionary<char, char>();
+            int count=s.Length;
+            for(int i=0;i<count;i++)
+            {
+                if(dic.ContainsKey(s[i]))
+                {
+                    if(dic[s[i]]!=t[i])
+                        return false;
+                }
+                else
+                {
+                    dic.Add(s[i],t[i]);
+                }
+            }
+        return true;
+    }
     }
 }
